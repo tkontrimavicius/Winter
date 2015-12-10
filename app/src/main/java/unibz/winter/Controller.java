6661690a8abd7386712d3f7efe217e3cfacdd2a2
haperlot <gmaximiliam15@gmail.com>
2015-12-10 15:13:54 +0100

    new strucure of the project

---
+++ app/src/main/java/unibz/winter/Controller.java
@@ -0,0 +1,45 @@
+package unibz.winter;
+
+/**
+ * Created by jetzt on 12/10/15.
+ */
+
+import java.util.ArrayList;
+import android.app.Application;
+
+import unibz.winter.model.ModelConversation;
+import unibz.winter.model.ModelUser;
+
+public class Controller extends Application{
+
+    private  ArrayList<ModelConversation> conversations = new ArrayList<ModelConversation>();
+    private ModelUser user = new ModelUser("Test User");
+
+
+
+   private void codeTextToMorse () {
+
+   }
+
+    private void decodeMorseToText () {
+
+    }
+
+    private void decodeSoundToText () {
+
+    }
+
+    private void decodeLightToText () {
+
+    }
+
+    private void codeSoundToText () {
+
+    }
+
+    private void codeLightToText () {
+
+    }
+
+
+}
